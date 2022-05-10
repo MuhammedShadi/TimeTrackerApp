1fe0219b9c52c488049c1a7e71d86693fc9ced54
Muhammed Abu Shadi <muhammed.samir.abushadi@gmail.com>
2022-05-10 11:28:21 +0300

    Fix IOS version with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:timetracker/screens/screens.dart';
 import 'package:timetracker/theme/theme.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(
       create: (_) => ThemeChanger(
